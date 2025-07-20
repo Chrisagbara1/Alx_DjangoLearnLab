a0730188ae0a77eb6dd2f6011d95562fc776f3c6
Chrisagbara1 <chrisagbara2023@mail.com>
2025-07-20 14:54:09 +0100

    urls views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Book
 
 def list_books(request):
-    books = Book.objects.all()  
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})  
 
 from django.views.generic.detail import DetailView
@@ -10,5 +10,5 @@ from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
