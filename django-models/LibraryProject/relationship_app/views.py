4d78190d42413a88f642c03ec3c76954b8386759
Chrisagbara1 <chrisagbara2023@mail.com>
2025-07-27 15:37:46 +0100

    permissions

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.decorators import permission_required
 from .models import Book, Library
+from .models import Library
 from .forms import BookForm
 
 
