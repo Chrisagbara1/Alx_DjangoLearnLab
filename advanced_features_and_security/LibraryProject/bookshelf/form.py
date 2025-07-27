d83213b9f66a42fdefed390f57ad6884590eb341
Chrisagbara1 <chrisagbara2023@mail.com>
2025-07-27 14:50:40 +0100

    security

---
+++ advanced_features_and_security/LibraryProject/bookshelf/form.py
@@ -0,0 +1,7 @@
+# bookshelf/forms.py
+
+from django import forms
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(label='Your Name', max_length=100)
+    email = forms.EmailField(label='Your Email')
