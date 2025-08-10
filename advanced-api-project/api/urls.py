7b87623b836d7e3e79372b03603d13d9ef55b6d2
Chrisagbara1 <chrisagbara2023@mail.com>
2025-08-10 10:50:50 +0100

    custum view

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import BookList, BookViewSet, BookUpdateView, BookDeleteView
+from .views import BookList, BookViewSet
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
-    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
-    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/create', BookViewSet.as_view({'post': 'create'}), name='book-create'),
+    path('books/update', BookViewSet.as_view({'put': 'update'}), name='book-update'),
+    path('books/delete', BookViewSet.as_view({'delete': 'destroy'}), name='book-delete'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('', include(router.urls)),
 ]
