27167fcde754c631cc5027f6532145f9cb1dd944
Chrisagbara1 <chrisagbara2023@mail.com>
2025-08-17 18:24:46 +0100

    initial setup and configuration for django blog

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+        'USER': '',      # still include these
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',      # keep field, even if empty
     }
 }
 
